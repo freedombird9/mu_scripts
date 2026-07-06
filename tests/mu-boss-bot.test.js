a08eeaf3408e8f07e4d8a09374a71d4c0dcac84c
Jack Zhang <jack.z@binance.com>
2026-07-06 21:06:53 +0800

    Harden MU boss bot test harness

--- tests/mu-boss-bot.test.js
+++ tests/mu-boss-bot.test.js
@@ -76,29 +76,70 @@ function makeStorage(seed = {}) {
   };
 }
 
+function makeConsoleStub() {
+  return {
+    log() {},
+    info() {},
+    warn() {},
+    error() {},
+    debug() {},
+  };
+}
+
+function makeMockDate(now) {
+  function MockDate(...args) {
+    if (!(this instanceof MockDate)) {
+      return new MockDate(...args).toISOString();
+    }
+
+    const value = args.length ? new global.Date(...args).getTime() : now;
+    Object.defineProperty(this, '_time', {
+      value,
+      enumerable: false,
+      configurable: false,
+      writable: false,
+    });
+  }
+
+  MockDate.now = function nowMock() {
+    return now;
+  };
+
+  MockDate.parse = function parseMock(value) {
+    return global.Date.parse(value);
+  };
+
+  MockDate.UTC = function utcMock(...args) {
+    return global.Date.UTC(...args);
+  };
+
+  MockDate.prototype.getUTCFullYear = function getUTCFullYear() {
+    return new global.Date(this._time).getUTCFullYear();
+  };
+
+  MockDate.prototype.getUTCMonth = function getUTCMonth() {
+    return new global.Date(this._time).getUTCMonth();
+  };
+
+  MockDate.prototype.getUTCDate = function getUTCDate() {
+    return new global.Date(this._time).getUTCDate();
+  };
+
+  MockDate.prototype.toISOString = function toISOString() {
+    return new global.Date(this._time).toISOString();
+  };
+
+  return MockDate;
+}
+
 function loadUserscript(root = buildEmptyRoot(), storage = makeStorage(), now = 1783339200000) {
   const documentElement = new FakeNode({ name: 'documentElement' });
+  const safeConsole = makeConsoleStub();
+  const MockDate = makeMockDate(now);
   const sandbox = {
     location: { href: 'https://cdn.qj2h5.jiuxiaokj.cn/mu2h5/h5-data/mu-release/index.html' },
-    console,
-    Date: class extends Date {
-      constructor(...args) {
-        if (args.length) return super(...args);
-        return new global.Date(now);
-      }
-
-      static now() {
-        return now;
-      }
-
-      static parse(value) {
-        return global.Date.parse(value);
-      }
-
-      static UTC(...args) {
-        return global.Date.UTC(...args);
-      }
-    },
+    console: safeConsole,
+    Date: MockDate,
     setInterval() { return 1; },
     clearInterval() {},
     setTimeout(fn) { return fn(); },
@@ -130,16 +171,24 @@ function loadUserscript(root = buildEmptyRoot(), storage = makeStorage(), now = 
 
   sandbox.window.window = sandbox.window;
   sandbox.window.document = sandbox.document;
+  sandbox.window.location = sandbox.location;
+  sandbox.window.console = sandbox.console;
+  sandbox.window.Date = sandbox.Date;
   sandbox.window.setInterval = sandbox.setInterval;
   sandbox.window.clearInterval = sandbox.clearInterval;
   sandbox.window.setTimeout = sandbox.setTimeout;
   sandbox.window.clearTimeout = sandbox.clearTimeout;
-  documentElement.appendChild = (script) => {
-    vm.runInContext(script.textContent, sandbox, { filename: 'injected-mu-boss-bot.js' });
-    return script;
+  sandbox.window.unsafeWindow = sandbox.window;
+  sandbox.unsafeWindow = sandbox.window;
+  documentElement.appendChild = (node) => {
+    if (node.tagName === 'SCRIPT') {
+      vm.runInContext(node.textContent, sandbox, { filename: 'injected-mu-boss-bot.js' });
+    }
+    return node;
   };
 
   vm.createContext(sandbox);
+  // Task 1 intentionally fails with ENOENT until Task 2 adds mu-boss-bot.user.js.
   vm.runInContext(fs.readFileSync(scriptPath, 'utf8'), sandbox, { filename: scriptPath });
   return { api: sandbox.window.__muBossBot, storage, sandbox };
 }
